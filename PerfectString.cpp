90fe074d99592fb10303a45a8f4ac4a78d9fb9aa
Mr-DareDevil <51032928+Mr-DareDevil@users.noreply.github.com>
2019-11-15 17:04:26 +0530

    Create PerfectString.cpp

---
+++ PerfectString.cpp
@@ -0,0 +1,17 @@
+Perfect String
+There is a string consisting of only ‘x’ and ‘y’ as the characters. Perfectness of a string is described as the maximum length substring of equal characters. You have given a number K which denotes the maximum number of characters that you can change. Find the maximum perfectness that you can generate by changing no more than K characters of the string.
+
+Input Format
+The first line contains an integer denoting the value of K. The next line contains a string having only ‘x’ and ‘y’ as the characters.
+
+Constraints
+2 ≤ N ≤ 10^5
+
+Output Format
+A single integer denoting the maximum perfectness achievable.
+
+Sample Input
+2
+xyxxyxyy
+Sample Output
+6
